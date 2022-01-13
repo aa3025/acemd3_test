d40f9033ad9f5a4b69ab79a3784958b020d90346
Alex Pedcenko <33809152+aa3025@users.noreply.github.com>
2022-01-13 23:37:23 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-module load python/last
+#module load python/last
 #conda create -n acemd_env -c conda-forge -c acellera acemd3
+
+source /share/apps/acemd3.sh
 conda activate acemd_env
 
-for dev in $(0 3); do
+for dev in 0 1 2 3; do
   mkdir -p GPU_${dev}
   cp template/* ./GPU_${dev}/
   cd GPU_${dev}
-  screen -A -m -d -S GPU_${dev} sh -c "acemd3 --device ${dev} input_acemd.inp | tee GPU_${dev}.log"
+  echo "Device $dev" > GPU_${dev}.log
+
+  echo "acemd3 --device ${dev} input_acemd.inp" >> GPU_${dev}.log
+  screen -A -m -d -S GPU_${dev} sh -c "acemd3 --device ${dev} input_acemd.inp >> GPU_${dev}.log"
   cd ../
+    sleep 1
 done
